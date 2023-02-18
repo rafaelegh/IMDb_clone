5571c12e5c6401a28149179200eaa53a87baf9dc
Rafael Gomez <rafael.gomez@leniolabs.com>
2023-02-18 17:52:56 -0300

    create Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import MenuItem from './MenuItem'
+
+const Header = () => {
+  return (
+    <div>
+        <div><MenuItem /></div>
+        <div><MenuItem /></div>
+        <div><MenuItem /></div>
+        <div><MenuItem /></div>
+    </div>
+  )
+}
+
+export default Header
