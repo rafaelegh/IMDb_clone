e85d6cf5af90d3c8be3059a6d4e4118c90577f26
Rafael Gomez <rafael.gomez@leniolabs.com>
2023-02-19 22:48:21 -0300

    handle error on root page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -11,7 +11,7 @@ export default async function Home({searchParams}) {
   );
 
   if (!res.ok) {
-    throw new Error("Failed to fetch data rafael"); // this will be caught by the error page and passed to the page as props
+    throw new Error("Failed to fetch data"); // this will be caught by the error page and passed to the page as props
   }
 
   const data = await res.json();
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,10 +1,10 @@
 export default function Results({ results }) {
     return (
         <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-6xl mx-auto py-4">
-            {results.map((result) => {
-                console.log(result)
-                return <div key={result.id}>{result.original_title || result.title || result.name}</div>
-            }
+            {results.map((result) => 
+                <div key={result.id}>
+                    {result.original_title || result.title || result.name}
+                </div>
             )}
         </div>
     );
