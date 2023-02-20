0d71068b7727610dc8930b7cd147aff01b4c8583
Rafael Gomez <rafael.gomez@leniolabs.com>
2023-02-19 22:33:55 -0300

    fetch data from tmdb api

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,26 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import Results from "@/components/Results";
 
-const inter = Inter({ subsets: ['latin'] })
+const API_KEY = process.env.API_KEY;
 
-export default function Home() {
+export default async function Home({searchParams}) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}`,
+    { next: { revalidate: 10000 } }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data rafael"); // this will be caught by the error page and passed to the page as props
+  }
+
+  const data = await res.json();
+
+  const results = data.results;
+
   return (
-    <h1 className='text-red-400'>HOME</h1>
-  )
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
